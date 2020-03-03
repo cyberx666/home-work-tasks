b119753a32c02551f39627c719e58ce4699824c5
cyberx666 <Toretto22ART>
2020-03-03 12:02:38 +0200

    Converter lecture 2,

--- src/main/java/com/hillel/lecture_2/SpeedConverter.java
+++ src/main/java/com/hillel/lecture_2/SpeedConverter.java
@@ -10,14 +10,14 @@ public class SpeedConverter {
     @Step
     public double speedKmToMi(double speedKm) {
 //        TODO implements result
-        double result = 0.0;
+        double result = speedKm*0.62140;
         return result;
     }
 
     @Step
     public double speedMiToKm(double speedMi) {
 //        TODO implements result
-        double result = 0.0;
+        double result = speedMi/0.62140;
         return result;
     }
 
