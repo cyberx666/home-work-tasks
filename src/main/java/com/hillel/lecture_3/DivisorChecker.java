8c4469bec4d565c5676993de00c353ffb2b4f9a4
cyberx666 <Toretto22ART>
2020-03-09 11:18:14 +0200

    Converter lecture 2,

--- src/main/java/com/hillel/lecture_3/DivisorChecker.java
+++ src/main/java/com/hillel/lecture_3/DivisorChecker.java
@@ -10,7 +10,14 @@ public class DivisorChecker {
     @Step
     public String checkDivisor(int divisor, int number) {
         //        TODO implements result
-        String result = "";
+        String result = "divisor";
+        if (divisor%number == 0){
+            return "The number" +" " + number + "" +" " + "is divisor of the number"+" " + divisor;
+
+        }
+        if(divisor%number != 0) {
+            return "The number" + " " + number + "" + " " + "is not divisor of the number" + " " + divisor;
+        }
 
         return result;
     }
